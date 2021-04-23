2aed8c93989a46e1186018683cba58e022c0a559
tpgent01 <73126336+tpgent01@users.noreply.github.com>
2021-04-23 16:51:14 -0500

    analyze browser resource allocation

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const webpack = require("webpack");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
 
 module.exports = {
     entry: './assets/js/script.js', 
@@ -7,11 +9,14 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.bundle.js'
       },
-      plugins:[
+      plugins: [
         new webpack.ProvidePlugin({
           $: "jquery",
           jQuery: "jquery"
         }),
-      ],
+        new BundleAnalyzerPlugin({
+          analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+        })
+      ], 
       mode: 'development' 
 };
