ab184159f8c521203cd79f2946e5ea569b4c139f
tpgent01 <73126336+tpgent01@users.noreply.github.com>
2021-04-24 11:42:49 -0500

    intercept fetch request

--- service-worker.js
+++ service-worker.js
@@ -23,7 +23,7 @@ self.addEventListener('install', function (e) {
         return cache.addAll(FILES_TO_CACHE)
       })
     )
-})
+});
 
 self.addEventListener('activate', function(e) {
     e.waitUntil(
@@ -43,4 +43,22 @@ self.addEventListener('activate', function(e) {
         );
       })
     );
-  });
+});
+
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+      caches.match(e.request).then(function (request) {
+        if (request) { // if cache is available, respond with cache
+          console.log('responding with cache : ' + e.request.url)
+          return request
+        } else {       // if there are no cache, try fetching request
+          console.log('file is not cached, fetching : ' + e.request.url)
+          return fetch(e.request)
+        }
+  
+        // You can omit if/else for console.log & put one line below like this too.
+        // return request || fetch(e.request)
+      })
+    )
+});
