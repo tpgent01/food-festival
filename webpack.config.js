295d350fc7879ff4c4f87c692a9461f5ff15c688
tpgent01 <73126336+tpgent01@users.noreply.github.com>
2021-04-24 11:35:23 -0500

    installed the service worker

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,8 @@ const config = {
               },
               publicPath: function(url) {
                 return url.replace('../', '/assets/');
-              }
+              },
+              esModule: false,
             }
           },
           {
